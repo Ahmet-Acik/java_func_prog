d44250281bf7943c305985fb5a30399cd23ccf05
drahmetacik <a.acik@icloud.com>
2025-05-05 23:30:09 +0100

    Add examples of Java's built-in functional interfaces in JDKFunctionalInterfaces

---
+++ src/main/java/org/ahmet/JDKFunctionalInterfaces.java
@@ -0,0 +1,24 @@
+package org.ahmet;
+
+import java.util.function.*;
+
+public class JDKFunctionalInterfaces {
+    public static void main(String[] args) {
+        // Example 1: Function - Transform an Integer to a String
+        Function<Integer, String> intToString = x -> "Number: " + x;
+        System.out.println(intToString.apply(42));
+
+        // Example 2: Consumer - Print a message
+        Consumer<String> printer = message -> System.out.println("Message: " + message);
+        printer.accept("Hello, Functional Interfaces!");
+
+        // Example 3: Supplier - Provide a random number
+        Supplier<Double> randomSupplier = Math::random;
+        System.out.println("Random number: " + randomSupplier.get());
+
+        // Example 4: Predicate - Check if a number is even
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        System.out.println("Is 10 even? " + isEven.test(10));
+        System.out.println("Is 15 even? " + isEven.test(15));
+    }
+}
