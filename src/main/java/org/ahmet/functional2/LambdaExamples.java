dbed3d287fe32f15e41ab0c398f11ae0bfe53a9a
drahmetacik <a.acik@icloud.com>
2025-05-06 11:54:54 +0100

    Add examples of using Consumer, Supplier, and Predicate functional interfaces in LambdaExamples

--- src/main/java/org/ahmet/functional2/LambdaExamples.java
+++ src/main/java/org/ahmet/functional2/LambdaExamples.java
@@ -1,6 +1,9 @@
 package org.ahmet.functional2;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class LambdaExamples {
     public static void main(String[] args) {
@@ -9,10 +12,25 @@ public class LambdaExamples {
         Runnable runnable = () -> System.out.println("This is a lambda expression!");
         runnable.run();
 
-        // 2. Lambda Expression Examples
+        // 2. Lambda Expression using Function functional interface Examples
         System.out.println("\n**Lambda Expression Examples**");
         Function<Integer, Integer> square = x -> x * x;
-        System.out.println("Square of 5: " + square.apply(5));
+        System.out.println("Square of 5: " + square.apply(5));  // Functional apply()
+
+        // Lambda Expression using Consumer functional interface
+        System.out.println("\n**Using Lambda with Consumer**");
+        Consumer<String> print = message -> System.out.println("Message: " + message);
+        print.accept("Hello, World!");  // Consumer accept()
+
+        // Lambda Expression using  Supplier functional interface
+        System.out.println("\n**Using Lambda with Supplier**");
+        Supplier<String> supplier = () -> "Hello from Supplier!";
+        System.out.println(supplier.get());  // Supplier get()
+
+        //Lambda Expression using Predicate functional interface
+        System.out.println("\n**Using Lambda with Predicate**");
+       Predicate<Integer> isEven = number -> number % 2 == 0;
+        System.out.println("Is 4 even? " + isEven.test(4));  // Predicate test()
 
         // Custom functional interface
         MathOperation addition = (a, b) -> a + b;
