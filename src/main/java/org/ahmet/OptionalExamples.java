0de73e052f6583e071c83a98b084ee1c8735de20
drahmetacik <a.acik@icloud.com>
2025-05-25 22:41:29 +0100

    add example for using or to provide an alternative Optional in OptionalExamples

--- src/main/java/org/ahmet/OptionalExamples.java
+++ src/main/java/org/ahmet/OptionalExamples.java
@@ -45,6 +45,10 @@ public class OptionalExamples {
         String valueFromSupplier = emptyOptional.orElseGet(() -> "Value from Supplier");
         System.out.println("Value from supplier: " + valueFromSupplier);
 
+        // Example 10: Using or to provide an alternative Optional
+        Optional<String> alternativeOptional = emptyOptional.or(() -> Optional.of("Alternative Value"));
+        alternativeOptional.ifPresent(value -> System.out.println("Alternative value: " + value));
+
 
     }
 }
