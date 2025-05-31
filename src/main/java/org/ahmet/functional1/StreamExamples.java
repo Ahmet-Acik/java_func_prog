2c314f2f57d53a7edbadd683cc16e6acddf7ed46
drahmetacik <a.acik@icloud.com>
2025-05-31 22:49:10 +0100

    refactor package structure for StreamExamples

--- src/main/java/org/ahmet/functional1/StreamExamples.java
+++ src/main/java/org/ahmet/functional1/StreamExamples.java
@@ -1,8 +1,7 @@
-package org.ahmet;
+package org.ahmet.functional1;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamExamples {
     public static void main(String[] args) {
